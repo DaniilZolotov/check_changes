49c2031bd96e3d2fb5e9bcbb273e21dfa2a2efd0
Daniil Zolotov <zolot@Zolotov.tivolion.local>
2024-11-01 11:06:37 +0300

    9:00 - 18:00

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from fake_useragent import UserAgent
 import os
 import telebot
 import shutil
-
-
+import datetime
 # Указываем путь к chromedriver.exe
 chromedriver_path = r"C:\Users\zolot\Desktop\try py\chrome\chromedriver.exe"
 url = 'https://fkr.eiasmo.ru'
 
 # БОТИК (Telegram Bot)
 bot = telebot.TeleBot('7707385206:AAGrRvyCLKS6dGO_u8wyJf63Q8NOVu-0ltk')
-TELEGRAM_CHAT_ID = '-4554782359'
+TELEGRAM_CHAT_ID = '-1002421991134'
 
 # Папка для скриншотов
 screenshot_folder = r'C:\Users\zolot\Desktop\try py\screenshot'  # папка скриншотов
@@ -45,70 +44,75 @@ def clear_directory(folder_path):
 
 def main():
     while True:
-        try:
-            # Настройки для Selenium
-            ua = UserAgent()
-            options = webdriver.ChromeOptions()
-            options.add_argument(f"user-agent={ua.random}")
-            options.add_argument("--disable-blink-features=AutomationControlled")
-           #headless mode
-            options.add_argument("--headless")
-            # Создаем объект Service и передаем путь к драйверу
-            service = Service(executable_path=chromedriver_path)
-            
-            # Инициализируем драйвер Chrome с использованием Service
-            driver = webdriver.Chrome(service=service, options=options)
-            
-            # Переход на страницу с камерами
-            driver.get(url=url)
-            time.sleep(2)
-            
-            # Авторизация на сайте
-            login_input = driver.find_element(By.CLASS_NAME, 'form_auth__input')
-            login_input.send_keys('isaev_im')
-            password_input = driver.find_element(By.NAME, 'password')
-            password_input.send_keys('12wq10e@1')
-            time.sleep(2)
-            driver.find_element(By.NAME, 'execute').click()
-            time.sleep(2)
-            
-            # Навигация к разделу видеонаблюдения
-            driver.find_element(By.XPATH, '//div[text()="Видеонаблюдение"]').click()
-            time.sleep(2)
-            driver.find_element(By.XPATH, '//span[text()="Список камер видеонаблюдения"]').click()
-            time.sleep(2)
-            
-            # Переключение на новое окно
-            driver.switch_to.window(driver.window_handles[-1])
-            driver.find_element(By.XPATH, '//div[text()="Список"]').click()
-            time.sleep(10)
-        
-            #очистка папки
-            clear_directory(screenshot_folder)
+        current_time = datetime.datetime.now().time()
+        start_time = datetime.time(9, 0)  # 09:00
+        end_time = datetime.time(18, 0)   # 18:00
 
-            # Создание скриншота
-            driver.maximize_window()
-            timestamp = time.strftime('%Y%m%d_%H%M%S')
-            screenshot_name = f'screenshot_{timestamp}.png'
-            screenshot_path = os.path.join(screenshot_folder, screenshot_name)
-            driver.save_screenshot(screenshot_path)
-            print(f'Скриншот сохранен: {screenshot_path}')
-
-            # Отправка скриншота в Telegram
-            send_screenshot(screenshot_path, message_text=f'Скриншот от {timestamp}')
+        if start_time <= current_time <= end_time:
+            try:
+                # Настройки для Selenium
+                ua = UserAgent()
+                options = webdriver.ChromeOptions()
+                options.add_argument(f"user-agent={ua.random}")
+                options.add_argument("--disable-blink-features=AutomationControlled")
+            #headless mode
+                options.add_argument("--headless")
+                # Создаем объект Service и передаем путь к драйверу
+                service = Service(executable_path=chromedriver_path)
+                
+                # Инициализируем драйвер Chrome с использованием Service
+                driver = webdriver.Chrome(service=service, options=options)
+                
+                # Переход на страницу с камерами
+                driver.get(url=url)
+                time.sleep(2)
+                
+                # Авторизация на сайте
+                login_input = driver.find_element(By.CLASS_NAME, 'form_auth__input')
+                login_input.send_keys('isaev_im')
+                password_input = driver.find_element(By.NAME, 'password')
+                password_input.send_keys('12wq10e@1')
+                time.sleep(2)
+                driver.find_element(By.NAME, 'execute').click()
+                time.sleep(2)
+                
+                # Навигация к разделу видеонаблюдения
+                driver.find_element(By.XPATH, '//div[text()="Видеонаблюдение"]').click()
+                time.sleep(2)
+                driver.find_element(By.XPATH, '//span[text()="Список камер видеонаблюдения"]').click()
+                time.sleep(2)
+                
+                # Переключение на новое окно
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.find_element(By.XPATH, '//div[text()="Список"]').click()
+                time.sleep(10)
             
-            # Закрываем драйвер перед следующим циклом
-            driver.quit()
-            
-            # Ждем 60 секунд перед следующим скриншотом
-            time.sleep(60)
+                #очистка папки
+                clear_directory(screenshot_folder)
 
-        except Exception as ex:
-            print(f"Произошла ошибка: {ex}")
-            # Закрываем драйвер в случае ошибки
-            driver.quit()
-            # Ждем некоторое время перед повторной попыткой
-            time.sleep(60)
+                # Создание скриншота
+                driver.maximize_window()
+                timestamp = time.strftime('%Yсс%m%d_%H%M%S')
+                screenshot_name = f'screenshot_{timestamp}.png'
+                screenshot_path = os.path.join(screenshot_folder, screenshot_name)
+                driver.save_screenshot(screenshot_path)
+                print(f'Скриншот сохранен: {screenshot_path}')
+
+                # Отправка скриншота в Telegram
+                send_screenshot(screenshot_path, message_text=f'Скриншот от {timestamp}')
+                
+                # Закрываем драйвер перед следующим циклом
+                driver.quit()
+                
+                # Ждем 60 секунд перед следующим скриншотом
+                time.sleep(1800)
+
+            except Exception as ex:
+                print(f"Произошла ошибка: {ex}")
+                # Закрываем драйвер в случае ошибки
+                driver.quit()
+                # Ждем некоторое время перед повторной попыткой
+                time.sleep(60)
 
 if __name__ == "__main__":
     main()
